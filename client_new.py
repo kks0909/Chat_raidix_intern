846c9260ae0829729392d08424cbcdd0b185ec5f
Ksenia <kk0909@mail.ru>
2022-04-05 21:43:34 +0300

    Shitty but stable. Kinda.

--- client_new.py
+++ client_new.py
@@ -15,6 +15,10 @@ NICK_APPROVED = '<NICK_APPROVED>'
 REMOVE = '<REM>'
 ADD = '<ADD>'
 MSG_tag = '<MSG>'
+SERVICE = '<SERVICE>'
+USERS = '<USERS>'
+USERS_END = '<USERS_END>'
+len_service = len(f'{SERVICE}')
 service_commands = [DISCONNECT_MSG, NICK_REQUEST, NICK_REQUEST_REP, ADD, REMOVE]
 
 SERVER = socket.gethostbyname(socket.gethostname())
@@ -28,7 +32,7 @@ nickname = ''
 
 def send():
 	while True:
-		text = input('Введите команду или адресата:')
+		text = input('Введите команду или адресата.\n')
 		if text == 'Help':
 			# Todo: показать справку
 			pass
@@ -36,6 +40,7 @@ def send():
 			# Todo: Запросить у сервера список пользователей
 			pass
 		elif text == 'Disconnect':
+			# Todo: обработать disconnect на сервере
 			client.send(DISCONNECT_MSG)
 			client.close()
 			break
@@ -43,23 +48,40 @@ def send():
 			print('Вы не можете отправить такое сообщение')
 		elif text in users:
 			destination = text
-			text = input('Введите сообщение')
+			text = input('Введите сообщение\n')
 			try:
 				# Todo: переписать ключи словаря на порты или нет
 				client.send(f'{MSG_tag}{destination};{client.getpeername()};{text}')
 			except:
 				pass
+		else:
+			print('Такого пользователя или команды не существует.')
 
 
 def receive():
+	"""
+	Основная часть приема сообщений
+	"""
 	while True:
 		try:
 			msg = client.recv(1024).decode(FORMAT)
+			print(f'Сообзеньице: {msg}\n')
 			# Начинается с системного сообщения
-			if msg.find(f'{ADD};') == 0:
-				users.append(msg[len(f'{ADD};'):])
-			elif msg.find(f'{REMOVE};') == 0:
-				users.remove(msg[len(f'{REMOVE};'):])
+			if msg.find(f'{SERVICE}') == 0:
+				# Смотрим, что следует за SERVICE
+				if msg.find(f'{ADD}') == len_service:
+					new_user = msg[len(f'{ADD}'):]
+					users.append(new_user)
+					print(f'Клиент подключился: {new_user}')
+				elif msg.find(f'{REMOVE};') == len_service:
+					rem_user = msg[len(f'{REMOVE}'):]
+					users.remove(rem_user)
+					print(f'Клиент отключился: {rem_user}')
+				elif msg.find(f'{USERS}') == len_service:
+					get_connected_users()
+				else:
+					print(f'Получено сообщение с пометкой системное, не удалось обработать:\n{msg}')
+			# Начинается с тега сообщения
 			elif msg.find(f'{MSG_tag};') == 0:
 				_, sender, text = msg.split(';', 2)
 				print(f'Получено сообщение от {sender}:\n{text}')
@@ -72,30 +94,63 @@ def receive():
 			break
 
 
+def get_connected_users():
+	"""
+	Получение уже подкюченных пользователей.
+	Структура сообщений:
+	<SERVICE><USERS> - уже получили
+	<SERVICE>username
+	...
+	<SERVICE><USERS_END>
+	"""
+	global users
+	while True:
+		msg = client.recv(1024).decode(FORMAT)[len_service:]
+		if msg != USERS_END:
+			users.append(msg)
+		else:
+			break
+	print(f'Подключенные клиенты: {", ".join(users)}')
+
+
 def welcome():
+	"""
+	Вызывается только при подключении
+	"""
 	accepted = False
+	global nickname
+	
 	while not accepted:
 		try:
 			msg = client.recv(1024).decode(FORMAT)
-			global nickname
-			if msg == NICK_REQUEST:
-				nickname = input('Введите свой псевдоним:')
-				client.send(nickname.encode(FORMAT))
-			elif msg == NICK_REQUEST_REP:
-				print('Такой псевдоним уже существует.')
-				nickname = input('Введите другой:')
-				client.send(nickname.encode(FORMAT))
-			elif msg == NICK_APPROVED:
-				accepted = True
-				print(f'Вы подключились к серверу как {nickname}')
+			if msg.find(f'{SERVICE}') == 0:
+				# Смотрим, что следует за SERVICE
+				reason = msg[len_service:]
+				if reason == NICK_REQUEST:
+					nickname = input('Введите свой псевдоним: ')
+					client.send(nickname.encode(FORMAT))
+				elif reason == NICK_REQUEST_REP:
+					print('Такой псевдоним уже существует.\n')
+					nickname = input('Введите другой: ')
+					client.send(nickname.encode(FORMAT))
+				elif reason == NICK_APPROVED:
+					print(f'Вы подключились к серверу как {nickname}')
+				elif msg.find(f'{USERS}') == len_service:
+					get_connected_users()
+				else:
+					print(f'Ну чет странное: {msg}\n')
+			else:
+				print(f'Ну чет очень странное: {msg}\n')
 		except:
 			print('Ошибка при задании псевдонима')
 			client.close()
 			return -1
+	return 1
 
 
 def start():
 	if welcome() != -1:
+		print('Начинаем слушать/отправлять')
 		# Starting Threads For Listening And Writing
 		receive_thread = threading.Thread(target=receive)
 		receive_thread.start()
@@ -104,4 +159,6 @@ def start():
 		write_thread.start()
 	else:
 		print('Все плохо')
+
+
 start()
--- server_new.py
+++ server_new.py
@@ -2,7 +2,7 @@
 
 import socket
 import threading
-
+# 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
 
 # TODO: Заголовок = адресат, адресант, текст
 # HEADER = 64
@@ -14,6 +14,9 @@ NICK_APPROVED = '<NICK_APPROVED>'
 REMOVE = '<REMOVE>'
 ADD = '<ADD>'
 MSG_tag = '<MSG>'
+SERVICE = '<SERVICE>'
+USERS = '<USERS>'
+USERS_END = '<USERS_END>'
 
 
 PORT = 5050
@@ -28,52 +31,68 @@ connections = {}
 
 def remove_client(nickname):
 	connections.pop(nickname)
-	broadcast(f'{REMOVE};{nickname}', nickname)
+	broadcast(REMOVE, nickname)
 	# f'{nickname}' отключилсяя
 
 
-def broadcast(msg, caused_client_nickname):
+def broadcast(reason, caused_client_nickname):
 	# Посылаем всем, кроме клиента, совершившего действие
 	for nickname, client in connections.items():
 		if nickname is not caused_client_nickname:
 			try:
-				client.send(msg)
+				service_send(reason, caused_client_nickname)
+				# client.send(msg.encode(FORMAT))
+				print('Посылка бродкаста')
 			except:
 				remove_client(nickname)
 
 
 def send(msg, destination_client):
 	#Todo: подтверждение получения
-	connections.get(destination_client).send(msg)
+	connections.get(destination_client).send(msg.encode(FORMAT))
+
+
+def service_send(reason, destination_client):
+	if destination_client in connections:
+		destination_client = connections.get(destination_client)
+	destination_client.send(f'{SERVICE}{reason}'.encode(FORMAT))
 
 
 def handle_client(nickname):
-	# print(type(nickname))
 	client = connections.get(nickname)
-	print(f'[NEW CONNECTION] {client.getpeername()} connected as {nickname}')
-	broadcast(f'{ADD};{nickname}', nickname)
-
 	while True:
 		try:
 			# TODO: возможность передавать большой объем
 			msg = client.recv(1024).decode(FORMAT)
+			print(msg)
 			destination_client = msg[:msg.find(';')]
 			send(msg, destination_client)
 		except:
 			remove_client(nickname)
+			print(f'[DELETE CONNECTION] {client.getpeername()} {nickname}')
 			break
 
 
 def welcome(conn):
 	# Добиваемся от пользователя псевдонима да так, чтоб он был уникальным
-	conn.send(NICK_REQUEST.encode(FORMAT))
+	service_send(NICK_REQUEST, conn)
+	print('Запрос ника')
 	nickname = conn.recv(1024).decode(FORMAT)
 	while connections.get(nickname) is not None:
-		conn.send(NICK_REQUEST_REP.encode(FORMAT))
+		service_send(NICK_REQUEST_REP, conn)
 		nickname = conn.recv(1024).decode(FORMAT)
 
-	conn.send(NICK_APPROVED.encode(FORMAT))
+	service_send(NICK_APPROVED, conn)
 	connections.update({nickname: conn})
+
+	# Посылаем уже подключенных к серверу пользователей
+	service_send(USERS, conn)
+	for user in connections:
+		service_send(user, conn)
+	service_send(USERS_END, conn)
+
+	print(f'[NEW CONNECTION] {conn.getpeername()} connected as {nickname}')
+	broadcast(ADD, nickname)
 
 	client_thread = threading.Thread(target=handle_client, args=(nickname,))
 	client_thread.start()
@@ -94,5 +113,10 @@ def listening():
 		welcome_thread.start()
 
 
-print('[STARTING]')
-listening()
+if __name__ == '__main__':
+	try:
+		print('[STARTING]')
+		listening()
+	except KeyboardInterrupt:
+		print('interrupted')
+		server.close()
